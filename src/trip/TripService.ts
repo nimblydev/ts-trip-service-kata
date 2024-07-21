800658302809fe21e1028c61d63e0c5e279652f3
Yoann Aubry <yoann@nimbly.dev>
2024-07-21 18:25:28 +0200

    TripService refactored

--- src/trip/TripService.ts
+++ src/trip/TripService.ts
@@ -1,34 +1,25 @@
-import UserNotLoggedInException from "../exception/UserNotLoggedInException";
-import User from "../user/User";
-import UserSession from "../user/UserSession";
-import Trip from "./Trip";
-import TripDAO from "./TripDAO";
+import UserNotLoggedInException from '../exception/UserNotLoggedInException';
+import User from '../user/User';
+import UserSession from '../user/UserSession';
+import Trip from './Trip';
+import TripDAO from './TripDAO';
 
 export default class TripService {
     public getTripsByUser(user: User): Trip[] {
-        let tripList: Trip[] = [];
-        const loggedUser: User = this.getLoggedInUser();
-        let isFriend: boolean = false;
-
-        if (loggedUser != null) {
-            for (const friend of user.getFriends()) {
-                if (friend === loggedUser) {
-                    isFriend = true;
-                    break;
-                }
-            }
-
-            if (isFriend) {
-                tripList = this.getTripsForUser( user);
-            }
-
-            return tripList;
-        } else {
+        if (this.getLoggedInUser() === null) {
             throw new UserNotLoggedInException();
         }
+
+        return user.isFriendsWith(this.getLoggedInUser())
+            ? this.getTripsForUser(user)
+            : this.noTrips();
     }
 
-    protected getTripsForUser( user: User) {
+    private noTrips(): Trip[] {
+        return [];
+    }
+
+    protected getTripsForUser(user: User) {
         return TripDAO.findTripsByUser(user);
     }
 
