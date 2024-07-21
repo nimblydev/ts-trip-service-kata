dfd3946ec488bbfa56d116e39912a78a74ccf37a
Yoann Aubry <yoann@nimbly.dev>
2024-07-21 17:17:41 +0200

    add a class style user builder and prettier to improve code

--- test/TripService.spec.ts
+++ test/TripService.spec.ts
@@ -1,58 +1,55 @@
-import UserNotLoggedInException from "../src/exception/UserNotLoggedInException";
-import Trip from "../src/trip/Trip";
-import TripService from "../src/trip/TripService";
-import User from "../src/user/User";
+import UserNotLoggedInException from '../src/exception/UserNotLoggedInException';
+import Trip from '../src/trip/Trip';
+import TripService from '../src/trip/TripService';
+import User from '../src/user/User';
+import { UserBuilder } from './UserBuilder';
 
-const GUEST:User = null;
-const UNUSED_USER:User = null;
+const GUEST: User = null;
+const UNUSED_USER: User = null;
 const REGISTERED_USER = new User();
 const ANOTHER_USER = new User();
 const TO_BRAZIL = new Trip();
 const TO_PARIS = new Trip();
 
-describe("TripService", () => {
-
+describe('TripService', () => {
     let tripService: TripService;
     let loggedInUser: User;
-    
+
     beforeEach(() => {
         tripService = new TestableTripService();
         loggedInUser = REGISTERED_USER;
     });
-   
 
-    describe("getTripsByUser should", () => {
-        it("should thow an exception when user is not logged", () => {
-            loggedInUser = GUEST;           
-            expect(()=> {
-                tripService.getTripsByUser(UNUSED_USER)
+    describe('getTripsByUser should', () => {
+        it('should thow an exception when user is not logged', () => {
+            loggedInUser = GUEST;
+            expect(() => {
+                tripService.getTripsByUser(UNUSED_USER);
             }).toThrow(UserNotLoggedInException);
-
         });
 
-        it("should not return any trip when users are not friends", () => {
-             const friend = new User();
-            friend.addFriend(ANOTHER_USER);
-            friend.addTrip(TO_BRAZIL);
+        it('should not return any trip when users are not friends', () => {
+            const friend: User = UserBuilder.aUser()
+                .friendsWith(ANOTHER_USER)
+                .withTrips(TO_BRAZIL)
+                .build();
 
             const trips = tripService.getTripsByUser(friend);
 
             expect(trips.length).toBe(0);
         });
 
-        it("should return trips when users are friends", () => {
-            const friend = new User();
-            friend.addFriend(ANOTHER_USER);
-            friend.addFriend(REGISTERED_USER);
-            friend.addTrip(TO_BRAZIL);
-            friend.addTrip(TO_PARIS);
+        it('should return trips when users are friends', () => {
+            const friend: User = UserBuilder.aUser()
+                .friendsWith(ANOTHER_USER, REGISTERED_USER)
+                .withTrips(TO_BRAZIL, TO_PARIS)
+                .build();
 
             const trips = tripService.getTripsByUser(friend);
 
             expect(trips.length).toBe(2);
         });
     });
-
 
     class TestableTripService extends TripService {
         /**
---
+++ test/UserBuilder.ts
@@ -0,0 +1,28 @@
+import Trip from '../src/trip/Trip';
+import User from '../src/user/User';
+
+export class UserBuilder {
+    private user: User;
+
+    static aUser(): UserBuilder {
+        return new UserBuilder();
+    }
+
+    constructor() {
+        this.user = new User();
+    }
+
+    friendsWith(...user: User[]): UserBuilder {
+        user.forEach((friend) => this.user.addFriend(friend));
+        return this;
+    }
+
+    withTrips(...trips: Trip[]): UserBuilder {
+        trips.forEach((trip) => this.user.addTrip(trip));
+        return this;
+    }
+
+    build(): User {
+        return this.user;
+    }
+}
